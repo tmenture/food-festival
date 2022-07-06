1d5bc3a4da1208a59c3f09e6fb18bd57981a9269
tmenture <thomasoxemail@gmail.com>
2022-07-05 20:56:40 -0400

    fixed service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,68 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
-const  webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-entry: {
-  app: './assets/js/script.js',
-  events: './assets/js/events.js',
-  schedule: './assets/js/schedule.js',
-  tickets: './assets/js/tickets.js'
-},
-output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-},
-module: {
-  rules: [
-    {
-      test: /\.jpg$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            esModule: false,
-            name(file) {
-              return "[path][name].[ext]"
-            },
-            publicPath: function(url) {
-              return url.replace('../', '/assets/')
+const config = {
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
+  output: {
+    path: path.join(__dirname + '/dist'),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath(url) {
+                return url.replace('../', '/assets/');
+              }
             }
+          },
+          {
+            loader: 'image-webpack-loader'
           }
-        },
-        {
-          loader: 'image-webpack-loader'
-        }
-      ]
-    }
-  ]
-},
-plugins:[
+        ]
+      }
+    ]
+  },
+  plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
-        analyzerMode: "static", // the report outputs to an html file in the dist folder
-    }),
+      analyzerMode: 'static'
+    })
+    // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
   ],
-mode: 'development'
+  mode: 'development'
 };
+
+module.exports = config;
